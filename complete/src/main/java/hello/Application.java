7cced9598419b640b306b77c03452be2af4ddcad
Artem Bilan <abilan@pivotal.io>
2016-06-29 09:54:23 -0400

    GH-13: Rename `blog` package to `hello`
    Fixes GH-13 (https://github.com/spring-guides/gs-integration/issues/13)
    
    Since all the Getting Started Guides uses the same project structure includes
    (e.g. https://github.com/spring-guides/getting-started-macros/blob/master/hide-show-gradle.adoc),
    we should change the actual directory in the project to the requested one.
    
    * Rename `blog` package to `hello` to meet general GS requirements
    * Fix `README.adoc` do not mention `/blog` directory, but `/hello`

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,11 +1,11 @@
-package blog;
+package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 public class Application {
     public static void main(String[] args) throws Exception {
-        ConfigurableApplicationContext ctx = new SpringApplication("/blog/integration.xml").run(args);
+        ConfigurableApplicationContext ctx = new SpringApplication("/hello/integration.xml").run(args);
         System.out.println("Hit Enter to terminate");
         System.in.read();
         ctx.close();
